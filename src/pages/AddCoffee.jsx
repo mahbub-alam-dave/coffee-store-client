dcab6e31801d236254edaa5dc0803ef58fb53343
mahbub-alam-dave <mahbubalamdave2.00@gmail.com>
2025-05-16 16:02:03 +0600

    initial commit to client

--- src/pages/AddCoffee.jsx
+++ src/pages/AddCoffee.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const AddCoffee = () => {
+
+    const handleAddCoffee = e => {
+        e.preventDefault()
+
+    }
     return (
         <div className='bg-[#F4F3F0] flex justify-center flex-col gap-8 px-12 py-12'>
             <h2 className='text-center text-2xl font-bold'>Add New Coffee</h2>
-            <form>
+            <form onSubmit={handleAddCoffee}>
                 <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6 md:gap-8 lg:gap-10 max-w-[1096px] mx-auto mb-4'>
                     <label htmlFor="name">Name <br />
                     <input type="text" name='name' placeholder='Enter coffee name' className='input w-full mt-1' />
